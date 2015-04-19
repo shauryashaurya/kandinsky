3bbb99fc8538b82d2ae20e43812b0f085bf4b430
Shaurya Agarwal <shauryashaurya@gmail.com>
2015-04-20 00:20:27 +0530

    starting over, notes and collecting my thoughts
    Next step - create a skeleton to test multiple algorithms and add tests

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-(function main(win,doc)
+(function main(win, doc)
 {
-	var self = this;
-	var win = window;
-	var doc = document;
+    var self = this;
+    var win = window;
+    var doc = document;
 
-	console.log("ready!");
+    console.log("win: ", win);
+    console.log("doc: ", doc);
+    console.log("self: ", self);
+    console.log("ready!");
 
-	/*Approach:
+    /*Approach:
 	1. Load image:
 		a. on input change, use file reader to capture file
 		b. create new image, define image onload to display image in canvas and start step2
@@ -21,6 +24,23 @@
 		c. move centroids to the mean/average of their respective clusters
 		d. did the centroids move much? if yes, goto b, else stop
 		e. publish results*/
+
+    /*principles:
+	1. no function should recieve things that it does not need
+	2. minimal modification of global state
+	3. try and not expose internal functionality
+	4. there's other algorithms coming your way, so engineer a plug-n-play approach*/
+
+    /* data structures:
+	colours: [r,g,b],[r,g,b],[r,g,b]... linear array of colour values
+	centroids: [r,g,b],[r,g,b],[r,g,b]...k linear array of K colour values
+	clusters: [[r,g,b],[r,g,b],...],[[r,g,b],[r,g,b],...]...k array of length k, 
+		each element is an array of all the colours
+		 that belong to the centorid at the respective index 
+		 in the centroids array
+	selected_colours: [r,g,b],[r,g,b],[r,g,b]...k linear array of K colour values
+	proportions: [p1, p2, p3, ..., pk] linear array of K values
+	*/
 
 
 
