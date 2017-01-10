2853c9cfad478d054416fb52fdedc986581e2161
Shaurya Agarwal <shauryashaurya@gmail.com>
2017-01-10 19:12:52 +0530

    adapting Kmeans ++
    It should not be working right now, but will do soon. :)

--- js/kandinsky.js
+++ js/kandinsky.js
@@ -20,7 +20,7 @@ function compute_kmeans(vectors, k) {
 	self.k = k;
 	self.vectorLength = self.vectors[0].length;
 	for (jobCycle = 0; jobCycle < 10; jobCycle++) {
-		console.log(".");
+		//console.log("jobCycle: ", jobCycle);
 		//console.log("jobCycle: ", jobCycle);
 		colorResults.push(compute_kmeans_jobcycle(vectors, k));
 	}
@@ -32,7 +32,7 @@ function compute_kmeans_jobcycle(vectors, k) {
 	self.clusters = [];
 	initCentroids();
 	self.oldCentroids = [];
-	while (!compareCentroids() && totaliter < 300) {
+	while (!compareCentroids() && totaliter < 500) {
 		totaliter++;
 		intertia[jobCycle] = totaliter;
 		//console.log("jobCycle: ", jobCycle,": iteration: ", totaliter);
@@ -62,6 +62,43 @@ function getRatioArr() {
 	return ratioArr;
 }
 
+function createVectorProbabilities(cArr) {
+	var caLen = cArr.length;
+	var vecLen = vectors.length;
+	var caWeights = [];
+	/*//console.log("createVectorProbabilities: cArr = ", JSON.stringify(cArr));
+	//console.log("createVectorProbabilities: caLen = ", caLen);
+	//console.log("createVectorProbabilities: vecLen = ", vecLen);*/
+	//each will be a sum of SED for all existing centroids
+	var i = 0;
+	var j = 0;
+	var vecWeightTemp = 0;
+	for (i = 0; i < vecLen; i++) {
+		vecWeightTemp = 0;
+		for (j = 0; j < caLen; j++) {
+			if ((typeof vectors[i] == "undefined" || !vectors[i]) || (typeof cArr[j] == "undefined" || !cArr[j])) {
+				//console.log("createVectorProbabilities: vectors[", i, "] = ", JSON.stringify(vectors[i]));
+				//console.log("createVectorProbabilities: cArr[", j, "] = ", JSON.stringify(cArr[j]));
+			}
+			vecWeightTemp = vecWeightTemp + squaredEuclideanDistance(vectors[i], cArr[j]);
+		}
+		caWeights.push(vecWeightTemp);
+	}
+	return caWeights;
+}
+
+function normalize(a) {
+	var totalWeight = a.reduce(function(a, b) {
+		return a + b;
+	}, 0);
+	var len = a.length;
+	var i = 0;
+	for (i = 0; i < len; i++) {
+		a[i] = a[i] / totalWeight;
+	}
+	return a;
+}
+
 function initCentroids() {
 	//take RGB data from the table and pick random centroids
 	//initialize another set of centroids array to zero
@@ -72,34 +109,37 @@ function initCentroids() {
 	totaliter = 0;
 	var i = 0;
 	var i2 = 0;
+	var ii = 0;
 	var randPointIndex = 0;
 	var pointrepeats = false;
 	var pointrepeatsIterationCount = 0;
 	var veclen = vectors.length;
 	var seedSimilarityToleranceInOneDimension = 32;
 	var seedSimilarityTolerance = 3 * seedSimilarityToleranceInOneDimension * seedSimilarityToleranceInOneDimension;
+	var centroidProbabilityDistribution = [];
+	var randomNum = 0;
+	var weight_sum = 0;
 	//console.log("initCentroids: seedSimilarityTolerance =", seedSimilarityTolerance);
 	self.centroids = [];
 	for (i = 0; i < k; i++) {
-		randPointIndex = Math.round(veclen * Math.random());
-		// check the centroid array for existing point
-		if (i > 0) {
-			pointrepeats = true;
-			pointrepeatsIterationCount = 0;
-			/*TODO: Fix this loop so that it doesn't run infinitely when there are fewer colors than K - 
-			for example, if there are only black pixels and white pixels in the picture but k is set to 7*/
-			while (pointrepeats && (pointrepeatsIterationCount < veclen)) {
-				randPointIndex = Math.round(veclen * Math.random());
-				pointrepeatsIterationCount++;
-				for (var j = 0; j < centroids.length; j++) {
-					// ensure that points are not exactly alike, use this only with monochrome images or images with low color contrast/vibrance.
-					//pointrepeats = pointrepeats && (squaredEuclideanDistance(centroids[j], vectors[randPointIndex]) === 0);
-					// ensure that points are not minimally similar - this should work for most images 
-					//pointrepeats = pointrepeats && (squaredEuclideanDistance(centroids[j], vectors[randPointIndex]) <= 1000);
-					// use tolerance here.
-					//seedSimilarityTolerance = 3 * seedSimilarityToleranceInOneDimension * seedSimilarityToleranceInOneDimension;
-					//console.log("initCentroids: seedSimilarityTolerance =", seedSimilarityTolerance);
-					pointrepeats = pointrepeats && (squaredEuclideanDistance(centroids[j], vectors[randPointIndex]) <= seedSimilarityTolerance);
+		// reset the probablity distribution
+		centroidProbabilityDistribution = [];
+		//console.log("initCentroids: i =", i);
+		//for the first time, just pick any random point
+		if (i == 0) {
+			randPointIndex = Math.round(veclen * Math.random());
+		} else if (i > 0) {
+			// create the probability distribution so that 
+			// points that are further away from the selected centroids have a greater probability of selection
+			centroidProbabilityDistribution = normalize(createVectorProbabilities(self.centroids));
+			randomNum = Math.random();
+			//console.log(random_num)
+			for (ii = 0; ii < veclen; ii++) {
+				weight_sum += centroidProbabilityDistribution[ii];
+				if (randomNum <= weight_sum) {
+					randPointIndex = ii;
+					//return list[ii];
+					break;
 				}
 			}
 		}
@@ -108,6 +148,7 @@ function initCentroids() {
 			self.centroids[i].push(self.vectors[randPointIndex][i2]);
 		}
 	}
+	//console.log("initCentroids: centroids = ", JSON.stringify(centroids));
 }
 
 function pickCentroids() {
@@ -119,34 +160,49 @@ function pickCentroids() {
 	// loop through this new array
 	// findPointwithleastrss for each
 	centclusters = [];
-	var lenclust = self.clusters.length;
+	// the clusters array stores the indices of centroids each point in the vectors array is associated with
+	var lenclust = self.clusters.length; // since clusters is an array of the same size as vectors, this should be equal to self.vectors.length
 	var i = 0;
-	oldCentroids = []
-		// first copy centorids to oldCentroids so that these can be compared later and also initialize centclusters
+	var i2 = 0;
+	oldCentroids = [];
+	var tempVector = [];
+	var tempIndex = 0;
+	// first copy centorids to oldCentroids so that these can be compared later and also initialize centclusters
 	for (i = 0; i < k; i++) {
 		oldCentroids.push(centroids.pop());
 		centclusters.push([]);
 	}
 	// now cycle through the clusters array to pick vectors for each centroid
-	for (i = 0; i < lenclust; i++) {
+	/*for (i = 0; i < lenclust; i++) {
 		centclusters[clusters[i]].push(JSON.parse(JSON.stringify(self.vectors[i])));
+	}*/
+	for (i = 0; i < lenclust; i++) {
+		tempIndex = clusters[i];
+		self.centclusters[tempIndex].push(self.vectors[i]);
 	}
 	for (i = 0; i < k; i++) {
 		centroids.push(findPointWithLeastRSS(centclusters[i]));
 	}
+	//console.log("pickCentroids: centroids: ", JSON.stringify(centroids));
 }
 
 function findPointWithLeastRSS(arr) {
+	//console.log("findPointWithLeastRSS: arr: ", JSON.stringify(arr));
 	var point = arr[0];
 	var rss_arr = [];
 	var min_rss = 0;
 	var pointIndex = 0;
+	console.log("findPointWithLeastRSS: 1 : arr[", pointIndex, "]: ", JSON.stringify(arr[pointIndex]));
+	if (typeof arr[pointIndex] == "undefined") {
+		console.log("findPointWithLeastRSS: 1 : arr: ", JSON.stringify(arr));
+	}
 	var arr_len = arr.length;
 	var i = 0;
 	// find rss for all points in the array
 	for (i = 0; i < arr_len; i++) {
 		rss_arr.push(residualSumOfSquares(arr, arr[i]))
 	}
+	//console.log("findPointWithLeastRSS: rss_arr: ", JSON.stringify(rss_arr));
 	min_rss = rss_arr[0];
 	for (i = 0; i < arr_len; i++) {
 		if (min_rss < rss_arr[i]) {
@@ -154,6 +210,7 @@ function findPointWithLeastRSS(arr) {
 			pointIndex = i;
 		};
 	}
+	console.log("findPointWithLeastRSS: arr[", pointIndex, "]: ", JSON.stringify(arr[pointIndex]));
 	return arr[pointIndex];
 }
 
@@ -176,10 +233,11 @@ function computeClusters() {
 	var minsedindex = 0;
 	clusters = [];
 	var tempsed = 0;
+	//console.log("computeClusters: self.centroids = ", JSON.stringify(self.centroids));
 	for (i = 0; i < numPoints; i++) {
 		sedArr = [];
 		for (j = 0; j < k; j++) {
-			tempsed = squaredEuclideanDistance(vectors[i], centroids[j]);
+			tempsed = squaredEuclideanDistance(self.vectors[i], self.centroids[j]);
 			sedArr.push(tempsed);
 		}
 		minsed = sedArr[0];
--- js/main.js
+++ js/main.js
@@ -1,80 +1,105 @@
 (function main(win, doc) {
-	var self = this,
-		win = window,
-		doc = document,
-		fileSelctorInput = doc.getElementById("fileSelctor"),
-		imagePreviewCanvas = doc.getElementById("imagePreview"),
-		imagePreviewCanvas2dContext = imagePreviewCanvas.getContext("2d"),
-		colorSwatchContainerDiv = doc.getElementById("colorSwatchContainer"),
-		k = 5;
-	
-	console.log("ready!");
-	// check if filereader and other apis are supported
-	if (window.File && window.FileReader && window.FileList && window.Blob && window.Worker) {
-		// Great success! All the File APIs are supported.
-	} else {
-		alert('This program requires File, FileReader, FileList, Blob and Worker APIs, some of which are not fully supported in your browser. Please upgrade your browser or switch to a new one and try again.');
-		return;
-	};
-	// xform data to cluster analysis friendly format
-	// from linear array to array of [r,g,b] chunks
-	var getPixelChunks = function(imgData) {
-		var i = -1,
-			len = imgData.length;
-		if (!imgData || imgData.length < 3) return [];
-		copy = [];
-		pixArr = [];
+    var self = this,
+        win = window,
+        doc = document,
+        fileSelctorInput = doc.getElementById("fileSelctor"),
+        imagePreviewCanvas = doc.getElementById("imagePreview"),
+        imagePreviewCanvas2dContext = imagePreviewCanvas.getContext("2d"),
+        colorSwatchContainerDiv = doc.getElementById("colorSwatchContainer"),
+        k = 5;
+    console.log("ready!");
+    // check if filereader and other apis are supported
+    if (window.File && window.FileReader && window.FileList && window.Blob && window.Worker) {
+        // Great success! All the File APIs are supported.
+    } else {
+        alert('This program requires File, FileReader, FileList, Blob and Worker APIs, some of which are not fully supported in your browser. Please upgrade your browser or switch to a new one and try again.');
+        return;
+    };
+    // xform data to cluster analysis friendly format
+    // from linear array to array of [r,g,b] chunks
+    var getPixelChunks = function(imgData) {
+        var i = -1,
+            len = imgData.length;
+        if (!imgData || imgData.length < 3) return [];
+        copy = [];
+        pixArr = [];
         // dumbcopy imageData Uint8ClampedArray to regular array so splice can be used
         while (++i < len) {
-			copy[i] = imgData[i];
-		}
-		//reset and loop again
-		i = -1;
-		len = copy.length;
-		while (++i < copy.length) {
-			pixArr[i] = copy.splice(0, 4); //rgba
-		}
-		return pixArr;
-	}
-	var analyzeImage = function() {
-			var imageData = imagePreviewCanvas2dContext.getImageData(0, 0, imagePreviewCanvas.width, imagePreviewCanvas.height),
-				pixelChunks = getPixelChunks(imageData.data),
-				imageColors;
-			imageColors = quantizeColors("kmeans", pixelChunks, k);
-			console.log("analyzeImage: imageColors: ", JSON.stringify(imageColors));
-		}
-		// load a file
-		// TODO:
-		// 1. Provide a drag and drop interface
-		// 2. Provide a progress bar for load / read / draw etc.
-		// 3. Promises based implementation
-	var handleFileSelection = function(evt) {
-			var files = [],
-				output = [],
-				selectedFile,
-				reader = new FileReader(),
-				image = new Image();
-			evt.stopPropagation();
-			evt.preventDefault();
-			files = evt.target.files;
-			selectedFile = files[0];
-			image.onload = function() {
-				imagePreviewCanvas.width = image.width;
-				imagePreviewCanvas.height = image.height;
-				imagePreviewCanvas2dContext.clearRect(0, 0, image.width, image.height);
-				imagePreviewCanvas2dContext.drawImage(image, 0, 0, image.width, image.height);
-				// with the image painted on scree, move on to clustering and painting the swatches on screen...
-				analyzeImage();
-			};
-			reader.onload = function(e) {
-				image.src = e.target.result;
-			};
-			reader.readAsDataURL(selectedFile);
-			//
-		}
-		//
-		// now wait for the user to select a file
-	fileSelctorInput.addEventListener('change', handleFileSelection, false);
+            copy[i] = imgData[i];
+        }
+        //reset and loop again
+        i = -1;
+        len = copy.length;
+        while (++i < copy.length) {
+            pixArr[i] = copy.splice(0, 4); //rgba
+        }
+        return pixArr;
+    }
+    var analyzeImage = function() {
+        var imageData = imagePreviewCanvas2dContext.getImageData(0, 0, imagePreviewCanvas.width, imagePreviewCanvas.height),
+            pixelChunks = getPixelChunks(imageData.data),
+            imageColors;
+        imageColors = quantizeColors("kmeans", pixelChunks, k);
+        console.log("analyzeImage: imageColors: ", JSON.stringify(imageColors));
+        drawColors(imageColors);
+    }
+
+    function drawColors(imageColors) {
+        var ii = 0;
+        var jj = 0;
+        for (ii = 0; ii < imageColors.length; ii++) {
+            for (jj = 0; jj < imageColors[ii]["colors"].length; jj++) {
+                var div = document.createElement("div");
+                div.style.width = "100px";
+                div.style.height = "100px";
+                div.style.display = 'inline-block';
+                div.style.background = "rgba(" + imageColors[ii]["colors"][jj].join(",") + ")";
+                //div.style.color = "white";
+                div.innerHTML = imageColors[ii]["colors"][jj].join(",");
+                colorSwatchContainerDiv.appendChild(div);
+            }
+            var div_spacer = document.createElement("div_spacer");
+            div_spacer.style.width = "100px";
+            div_spacer.style.height = "30px";
+            div_spacer.style.background = "rgba(255,255,255,255)";
+            div_spacer.style.display = 'block';
+            //div_spacer.style.color = "white";
+            div_spacer.innerHTML = "-----";
+            colorSwatchContainerDiv.appendChild(div_spacer);
+        }
+    }
+    // load a file
+    // TODO:
+    // 1. Provide a drag and drop interface
+    // 2. Provide a progress bar for load / read / draw etc.
+    // 3. Promises based implementation
+    var handleFileSelection = function(evt) {
+            var files = [],
+                output = [],
+                selectedFile,
+                reader = new FileReader(),
+                image = new Image();
+            evt.stopPropagation();
+            evt.preventDefault();
+            files = evt.target.files;
+            selectedFile = files[0];
+            image.onload = function() {
+                imagePreviewCanvas.width = image.width;
+                imagePreviewCanvas.height = image.height;
+                imagePreviewCanvas2dContext.clearRect(0, 0, image.width, image.height);
+                imagePreviewCanvas2dContext.drawImage(image, 0, 0, image.width, image.height);
+                // with the image painted on scree, move on to clustering and painting the swatches on screen...
+                analyzeImage();
+            };
+            reader.onload = function(e) {
+                image.src = e.target.result;
+            };
+            reader.readAsDataURL(selectedFile);
+            //
+        }
+        //
+        // now wait for the user to select a file
+    fileSelctorInput.addEventListener('change', handleFileSelection, false);
 })(window, document);
 /*Approach:
     1. Load image:
